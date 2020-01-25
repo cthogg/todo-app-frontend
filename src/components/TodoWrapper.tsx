815dd9cfb65db85444a480b360f7aa13e4ade318
cthogg <cthogg@users.noreply.github.com>
2020-01-25 15:35:08 +0100

    feat: add delete todo

--- src/components/TodoWrapper.tsx
+++ src/components/TodoWrapper.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import TodoForm from "./TodoForm";
 import MultipleTodos from "./MultipleTodos";
 import { Todo } from "../types";
-import { mockTodos } from "../App";
 import axios from "axios";
 
 interface TodoWrapperProps {
@@ -15,10 +14,11 @@ const TodoWrapper: React.FunctionComponent<TodoWrapperProps> = ({
   //TODO:this works but is a hack to force a rerender.
   const [num, setNum] = useState<number>(1);
   const fetchTodoRoute = "todos";
-
-  const apiHost = "http://localhost:4000/";
-  // const apiHost = 'http://todo-app-backend-66315.herokuapp.com/'
+  //TODO: make this an environmental variable
+  // const apiHost = "http://localhost:4000/";
+  const apiHost = "http://todo-app-backend-66315.herokuapp.com/";
   const POST_TODOS = `${apiHost}${fetchTodoRoute}`;
+  const DELETE_TODO = `${apiHost}${fetchTodoRoute}`;
 
   useEffect(() => {
     const FETCH_TODOS = `${apiHost}${fetchTodoRoute}`;
@@ -33,10 +33,19 @@ const TodoWrapper: React.FunctionComponent<TodoWrapperProps> = ({
 
   const postTodo = async (todo: Todo) => {
     const result = await axios.post(POST_TODOS, todo);
+    console.log("result: ", result);
     //TODO: show error if error
     setNum(num + 1);
   };
 
+  const deleteTodo = async (todoId: string) => {
+    const result = await axios.delete(`${DELETE_TODO}/${todoId}`);
+    console.log("result: ", result);
+    //TODO: show error if error
+    setNum(num + 1);
+  };
+
+  // TODO: add these mocks as a switch
   const postTodoMock = todo => {
     var newTodos = todos;
     newTodos.push(todo);
@@ -45,19 +54,20 @@ const TodoWrapper: React.FunctionComponent<TodoWrapperProps> = ({
     setNum(num + 1);
   };
 
+  const deleteTodoMock = (id: string) => {
+    console.log("todos", todos);
+    //TODO: add test here
+    const newTodos = todos.filter(todo => todo.id !== id);
+    console.log("newTodos: ", newTodos);
+    setTodos(newTodos);
+    setNum(num + 1);
+  };
   return (
     <>
       <TodoForm addTodo={todo => postTodo(todo)} />
       {todos.length > 0 && (
         <MultipleTodos
-          onDelete={(id: string) => {
-            console.log("todos", todos);
-            //TODO: add test here
-            const newTodos = todos.filter(todo => todo.id !== id);
-            console.log("newTodos: ", newTodos);
-            setTodos(newTodos);
-            setNum(num + 1);
-          }}
+          onDelete={(id: string) => deleteTodo(id)}
           todos={todos}
         />
       )}
