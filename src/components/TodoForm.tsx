3f09672a6a95a05cf07d38e95864fa33cb806d2a
cthogg <cthogg@users.noreply.github.com>
2020-01-25 14:47:35 +0100

    feat: form error handling, reset on submit

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -15,29 +15,46 @@ const TodoForm: React.FunctionComponent<TodoFormProps> = ({
   addTodo
 }: TodoFormProps): JSX.Element => {
   const [count, setCount] = useState<number>(0);
-
+  const DEFAULT_DATE = "";
   return (
     <div>
       <h1>Add a TODO</h1>
       <Formik
         initialValues={{
-          title: "Title",
-          description: "Description",
-          date: "2020-01-01"
+          title: "",
+          description: "",
+          date: DEFAULT_DATE
         }}
-        onSubmit={(values, { setSubmitting }) => {
+        validate={values => {
+          const errors = {};
+          if (values.title === "") {
+            errors["title"] = "Title Required";
+          }
+          if (values.date === DEFAULT_DATE) {
+            errors["date"] = "Date Required";
+          }
+          if (values.description === "") {
+            errors["description"] = "Description Required";
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting, resetForm }) => {
           var submittedValues = values;
           submittedValues["id"] = generateNewId();
           console.log("submittedValues: ", submittedValues);
           addTodo(submittedValues);
+          resetForm();
           setSubmitting(false);
         }}
       >
         {({ isSubmitting }) => (
           <Form>
             <Field type="text" name="title" />
+            <ErrorMessage name="title" component="div" />
             <Field type="text" name="description" />
+            <ErrorMessage name="description" component="div" />
             <Field type="date" name="date" />
+            <ErrorMessage name="date" component="div" />
             <button type="submit" disabled={isSubmitting}>
               Submit
             </button>
