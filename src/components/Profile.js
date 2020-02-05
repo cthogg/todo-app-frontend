5cda751c0465e006c109533fb282088c4f91c74b
cthogg <cthogg@users.noreply.github.com>
2020-02-05 14:03:04 +0100

    feat: add please login

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,13 +7,11 @@ const Profile = () => {
   const { loading, user } = useAuth0();
 
   if (loading || !user) {
-    return <div>Loading...</div>;
+    return <div>Please Log In</div>;
   }
 
   return (
     <Fragment>
-      <img src={user.picture} alt="Profile" />
-
       <h2>{user.name}</h2>
       <p>{user.email}</p>
       <code>{JSON.stringify(user, null, 2)}</code>
